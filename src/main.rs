1dd5f00dda35f7ecc575fad5f7df5c9459d9bee3
Diaa J <ditek@users.noreply.github.com>
2024-03-25 01:04:41 +0100

    netwrok-perf: convert test results to json

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
 use clap::Parser;
 use regex::Regex;
+use serde::Serialize;
+use serde_json::Value;
 use std::net::{AddrParseError, Ipv4Addr};
 use std::process::Command;
-use serde_json::Value;
 
 /// Run network latency and throughput tests
 #[derive(Parser)]
@@ -17,23 +18,40 @@ struct Cli {
 
 #[derive(Debug)]
 #[allow(dead_code)]
+#[derive(Serialize)]
 struct Latency {
     min: f64,
     avg: f64,
     max: f64,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Serialize)]
 struct IperfResult {
-    remote_host: String,
+    server: String,
     timestamp: String,
-    bitrate_sent_mb: f64,
-    bitrate_received_mb: f64,
+    uplink: f64,
+    downlink: f64,
     duration: i64,
+}
+
+#[derive(Debug, Serialize)]
+struct TestResult {
+    id: String,
+    latency: Option<Latency>,
+    iperf: Option<IperfResult>,
+}
+
+#[derive(Debug, Serialize)]
+struct TestResults {
+    results: Vec<TestResult>,
 }
 
 fn validate_ip(ip: &str) -> Result<Ipv4Addr, AddrParseError> {
     ip.parse::<Ipv4Addr>()
+}
+
+fn trim_float(x: f64) -> f64 {
+    format!("{:.1}", x).parse().unwrap()
 }
 
 #[allow(dead_code)]
@@ -62,6 +80,7 @@ fn run_ping(ip: Ipv4Addr) -> Result<Latency, String> {
             max: caps[3].parse().expect("Failed to parse max rtt"),
         };
         println!("{:?}", latency);
+        println!("{output_str}");
         return Ok(latency);
     } else {
         eprintln!("Failed to parse ping output:\n{}", output_str);
@@ -84,27 +103,40 @@ fn run_iperf3(ip: Ipv4Addr) -> Result<IperfResult, String> {
     let output_json = String::from_utf8_lossy(&output.stdout);
     let json: Value = serde_json::from_str(&output_json).expect("Failed to parse iperf3 output");
 
+    let ul = json["end"]["sum_sent"]["bits_per_second"].as_f64().unwrap();
+    let dl = json["end"]["sum_received"]["bits_per_second"].as_f64().unwrap();
     let result = IperfResult {
-        remote_host: json["start"]["connecting_to"]["host"].to_string(),
-        timestamp: json["start"]["timestamp"]["time"].to_string(),
+        server: json["start"]["connecting_to"]["host"].as_str().unwrap().to_string(),
+        timestamp: json["start"]["timestamp"]["time"].as_str().unwrap().to_string(),
         duration: json["start"]["test_start"]["duration"].as_i64().unwrap(),
-        bitrate_sent_mb: json["end"]["sum_sent"]["bits_per_second"].as_f64().unwrap() * 1e-6,
-        bitrate_received_mb: json["end"]["sum_received"]["bits_per_second"]
-            .as_f64()
-            .unwrap()
-            * 1e-6,
+        uplink: trim_float(ul * 1e-6),
+        downlink: trim_float(dl * 1e-6),
     };
-    println!("{:?}", result);
+    // println!("{:?}", result);
     Ok(result)
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args = Cli::parse();
+    let mut result = TestResult {
+        id: "test".to_string(),
+        latency: None,
+        iperf: None,
+    };
     if let Some(ip) = args.ping_ip {
-        run_ping(ip)?;
+        match run_ping(ip) {
+            Ok(latency) => result.latency = Some(latency),
+            Err(e) => eprintln!("Failed to run ping test: {}", e),
+        }
     }
     if let Some(ip) = args.iperf_ip {
-        run_iperf3(ip)?;
+        match run_iperf3(ip) {
+            Ok(iperf) => result.iperf = Some(iperf),
+            Err(e) => eprintln!("Failed to run iperf3 test: {}", e),
+        }
     }
+
+    let json = serde_json::to_string(&result)?;
+    println!("{}", json);
     Ok(())
 }
